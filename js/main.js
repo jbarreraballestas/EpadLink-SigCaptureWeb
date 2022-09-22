a80d588c6fb4819506a02ea34cef6e76db717fb5
JBarreraBallestas <jbarreraballestas@correo.unicordoba.edu.co>
2022-09-22 10:02:20 -0500

    Validar si la extensión del navegador se encuentra instalada

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
   const chromium = !!navigator.userAgentData && navigator.userAgentData.brands.some(data => data.brand == 'Chromium');
   if (!chromium) {
     Swal.fire({
@@ -7,7 +7,14 @@ document.addEventListener('DOMContentLoaded',()=>{
       html: '<p>El navegador que estás usando no es compatible!</p>',
     });
   } else {
-    
+    var isInstalled = document.documentElement.getAttribute('SigCaptureWebExtension-installed');
+    if (!isInstalled) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Ups...',
+        html: '<p>No tienes instalada la extensión del navegador!</p><a target="_blank" href="https://chrome.google.com/webstore/detail/epadlink-sigcaptureweb-sd/idldbjenlmipmpigmfamdlfifkkeaplc">Instalar</a>',
+      });
+    }
   }
 
 })
