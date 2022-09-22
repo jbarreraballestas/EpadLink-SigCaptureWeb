154f7110b47c0aee4f89729cf55cdf52a4e7a247
JBarreraBallestas <jbarreraballestas@correo.unicordoba.edu.co>
2022-09-22 12:03:02 -0500

    Make Sweetalert optional

--- js/main.js
+++ js/main.js
@@ -2,22 +2,33 @@
 window.addEventListener('load', () => {
   const chromium = !!navigator.userAgentData && navigator.userAgentData.brands.some(data => data.brand == 'Chromium');
   if (!chromium) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Ups...',
-      html: '<p>El navegador que estás usando no es compatible, por favor usa Chrome o Microsoft Edge!</p>',
-    });
+    if (window.Swal) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Ups...',
+        html: '<p>El navegador que estás usando no es compatible, por favor usa Chrome o Microsoft Edge!</p>',
+      });
+    } else {
+      alert('El navegador que estás usando no es compatible, por favor usa Chrome o Microsoft Edge!');
+    }
   } else {
     if (!document.documentElement.getAttribute('sigcapturewebextension-installed')) {
-      Swal.fire({
-        icon: 'warning',
-        title: 'Ups...',
-        html: '<p>No tienes instalada la extensión del navegador!</p><a target="_blank" href="https://chrome.google.com/webstore/detail/epadlink-sigcaptureweb-sd/idldbjenlmipmpigmfamdlfifkkeaplc">Instalar</a>',
-      });
-      setInterval(()=>{
+      if (window.Swal) {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Ups...',
+          html: '<p>No tienes instalada la extensión del navegador!</p><a target="_blank" href="https://chrome.google.com/webstore/detail/epadlink-sigcaptureweb-sd/idldbjenlmipmpigmfamdlfifkkeaplc">Instalar</a>',
+        });
+      } else {
+        if (confirm('No tienes instalada la extensión del navegador, deseas instalarla?')) {
+          window.open('https://chrome.google.com/webstore/detail/epadlink-sigcaptureweb-sd/idldbjenlmipmpigmfamdlfifkkeaplc', '_blank');
+        }
+      }
+
+      setInterval(() => {
         location.reload();
-      },10000);
-    }else{
+      }, 10000);
+    } else {
 
     }
   }
